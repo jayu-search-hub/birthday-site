6517058f9fa5f1fffb6b64d81378262b60707668
jayu-search-hub <160621944+jayu-search-hub@users.noreply.github.com>
2025-12-25 03:59:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showNote() {
+  alert("Glad for the moments, the conversations, and the time we share 🌷");
+}
+
+const overlay = document.getElementById("overlay");
+
+overlay.addEventListener("click", () => {
+  overlay.classList.add("hide");
+});
